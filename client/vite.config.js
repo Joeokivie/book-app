ce345a39305fe41e050398adb7f156722ea3b7a6
joeokivie <josephokivie@hotmail.com>
2024-01-18 21:23:09 -0500

    attempting another heroku deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,15 +14,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/graphql': {
-        target: 'http://localhost:5000',
-        secure: false,
-        changeOrigin: true
-      }
-    }
-  }
+  // server: {
+  //   port: 3000,
+  //   open: true,
+  //   proxy: {
+  //     '/graphql': {
+  //       target: 'http://localhost:5000',
+  //       secure: false,
+  //       changeOrigin: true
+  //     }
+  //   }
+  // }
 })
